08c2e2a89b741372343293a36599f5b00825381e
mariesergeeva <88836991+mariesergeeva@users.noreply.github.com>
2022-07-05 16:22:36 +0200

    Add files via upload

---
+++ модуль 1/2. Синтаксис/index.js
@@ -0,0 +1,15 @@
+//JavaScript-код с созданием переменных
+/* 
+Автор:
+Мария Сергеева
+*/
+const myName = 'Mariia'
+let mySurname = 'Sergeeva'
+let myFavoriteDrink = 'Iced Latte'
+let myFavoriteAnimal = 'Rook'
+const myFavoriteProgrammingLanguage = 'JavaScript'
+console.log(myName)
+console.log(mySurname)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myFavoriteProgrammingLanguage)
