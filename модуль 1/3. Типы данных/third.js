a236f203493b9c10c97f3016e84147988122df11
mariesergeeva <88836991+mariesergeeva@users.noreply.github.com>
2022-07-07 12:43:37 +0200

    Update third.js

--- модуль 1/3. Типы данных/third.js
+++ модуль 1/3. Типы данных/third.js
@@ -11,8 +11,8 @@ const bestProgrammingLanguage2 = "Java"
 console.log('Переменную с const переопределить нельзя')
 console.log(bestProgrammingLanguage1, typeof bestProgrammingLanguage1)
 console.log(bestProgrammingLanguage2, typeof bestProgrammingLanguage2)
-let initialValue1;
-let initialValue2 = 0;
+let initialValue1
+let initialValue2 = 0
 initialValue2 = initialValue1
 console.log(initialValue2, typeof initialValue2)
 console.log('var является устаревшим')
@@ -20,7 +20,7 @@ let isJavaScriptProgrammer1 = true
 let isJavaScriptProgrammer2 = false
 isJavaScriptProgrammer2 = isJavaScriptProgrammer1
 console.log(isJavaScriptProgrammer2, typeof isJavaScriptProgrammer2)
-let helloText1 = "Hello!";
-let helloText2 = "Привет!";
+let helloText1 = "Hello!"
+let helloText2 = "Привет!"
 helloText2 = helloText1
 console.log(helloText2, typeof helloText2)
