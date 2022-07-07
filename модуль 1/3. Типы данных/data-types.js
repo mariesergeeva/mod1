38deadaddcd757331ba87ecf99baa4c23a1e9009
mariesergeeva <88836991+mariesergeeva@users.noreply.github.com>
2022-07-07 11:47:15 +0200

    Add files via upload

---
+++ модуль 1/3. Типы данных/data-types.js
@@ -0,0 +1,20 @@
+const myName = 'Mariia'
+const age = 28;
+const someBigNumber = 666n
+const isBigNumber = true
+const bigNumberName = null
+const bigNumber = {
+    bigNumberName: null,
+    someBigNumber: 666n,
+    isBigNumber: true
+}
+const id = Symbol("id")
+let notConst
+console.log("name", myName, typeof myName);
+console.log("age", age, typeof age);
+console.log('big number', someBigNumber, typeof someBigNumber)
+console.log('is it big number?', isBigNumber, typeof isBigNumber)
+console.log('name of big number', bigNumberName, typeof bigNumberName)
+console.log('big object', bigNumber, typeof bigNumber)
+console.log('symbol', id, typeof id)
+console.log('notConst', notConst, typeof notConst)
---
+++ модуль 1/3. Типы данных/index.js
@@ -0,0 +1,9 @@
+let someNumber = 111
+let someString = 'string'
+let isBoolean = true
+someNumber = '111'
+someString = 12345
+isBoolean = null
+alert(someNumber)
+alert(someString)
+alert(isBoolean)
